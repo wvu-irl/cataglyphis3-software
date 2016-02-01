2e39e6a8c7bb87e0be5aff3ba3c2d1f067254865
Matt Gramlich <mpgramlich@mix.wvu.edu>
2016-02-01 15:10:06 -0500

    Adding new resource files and updating GUI to a new test version that demonstrates field selection and data displaying.

--- cataglyphis_gui/qt_project_src/cataglyphis_gui/cataglyphis_gui.cpp
+++ cataglyphis_gui/qt_project_src/cataglyphis_gui/cataglyphis_gui.cpp
@@ -1,33 +1,38 @@
 #include "cataglyphis_gui.h"
 #include "ui_cataglyphis_gui.h"
-#include <QGraphicsPixmapItem>
 #include <stdio.h>
 
-QGraphicsScene* scene;
-QGraphicsView* view;
-QGraphicsPixmapItem* item;
-QImage* field_pic;
 
 Cataglyphis_Gui::Cataglyphis_Gui(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Cataglyphis_Gui)
 {
+    scene = new QGraphicsScene();
+    //field_pic = new QImage[4];
+    field_pic[0] = new QImage(":/field_pictures/resources/pattern_test.jpg");
+    field_pic[1] = new QImage(":/field_pictures/resources/institute_park.jpg");
     ui->setupUi(this);
 }
 
 Cataglyphis_Gui::~Cataglyphis_Gui()
 {
+    //printf("%p, %p, %p, %p\n", scene, view, item, field_pic);
     delete ui;
+    //printf("%p, %p, %p, %p\n", scene, view, item, field_pic);
 }
 
 void Cataglyphis_Gui::on_fieldSelector_currentIndexChanged(int index)
 {
     printf("Index Changed %d\n", index);
-    field_pic = new QImage(":/field_pictures/resources/institute_park.jpg");
-    scene = new QGraphicsScene();
-    view = new QGraphicsView(scene);
-    item = new QGraphicsPixmapItem(QPixmap::fromImage(*field_pic));
+
+    scene->clear();
+    item = new QGraphicsPixmapItem(QPixmap::fromImage(*field_pic[index]));
+
+    //need to load new info about the map to display the roer on it.
+    //where coordinate 0,0 is
+    //the distance each pixel represents on the map
     ui->fieldDisplay->setScene(scene);
     scene->addItem(item);
+    cataglyphisRect = scene->addRect(QRectF(0,0,50,50));
     ui->fieldDisplay->show();
 }
--- cataglyphis_gui/qt_project_src/cataglyphis_gui/cataglyphis_gui.h
+++ cataglyphis_gui/qt_project_src/cataglyphis_gui/cataglyphis_gui.h
@@ -2,6 +2,8 @@
 #define CATAGLYPHIS_GUI_H
 
 #include <QMainWindow>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsView>
 
 namespace Ui {
 class Cataglyphis_Gui;
@@ -20,6 +22,11 @@ private slots:
 
 private:
     Ui::Cataglyphis_Gui *ui;
+    QGraphicsScene* scene;
+    //QGraphicsView* view;
+    QGraphicsPixmapItem* item;
+    QImage* field_pic[4];
+    QGraphicsRectItem* cataglyphisRect;
 };
 
 #endif // CATAGLYPHIS_GUI_H
