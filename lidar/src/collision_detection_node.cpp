7e7ef516bb085004067e9126ac28b24579d5b5fe
Jared Strader <jared.l.strader@gmail.com>
2016-03-03 09:24:21 -0500

    add msg to collision detection node

--- lidar/src/collision_detection_node.cpp
+++ lidar/src/collision_detection_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <iostream>
+#include <lidar/CollisionOut.h>
 
 using namespace std;
 
@@ -20,18 +21,71 @@ public:
 	// }
 };
 
+//subscribe to state machine info
+class RobotControlSubscriber
+{
+private:
+	ros::Subscriber sub_sm;
+	ros::NodeHandle node;
+
+	// void getExecCallback(const robot_control::ExecStateMachineInfo::ConstPtr &msg)
+	// {
+	// 	if(msg->var == 1) //logic to decide to run collision detection or not
+	// 	{
+	// 		this->check_for_collisions=1;
+	// 	}
+	// 	else
+	// 	{
+	// 		this->check_for_collisions=0;
+	// 	}
+	// }
+public:
+	int check_for_collisions;
+	RobotControlSubscriber()
+	{
+		// sub_sm = node.subscribe("/control/execinfo/execinfo", 1, &RobotControlSubscriber::getExecCallback, this);
+		check_for_collisions = 1; //change to 0 once subscribed to robot control
+	}
+};
+
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "collision_detection_node");
+	ROS_INFO("collision_detection_node running...");
 	ros::NodeHandle nh;
 	ros::Rate loop_rate(50);
+	ros::Publisher pub_col = nh.advertise<lidar::CollisionOut>("lidar/collisiondetectionout/collisiondetectionout",1);
 
-	ROS_INFO("collision_detection_node running...");
-
+	//subscribers
 	VelodyneSubscriber velodyne;
+	RobotControlSubscriber robot_control;
+
+	//output messages
+	lidar::CollisionOut msg_CollisionOut;
 
 	while(ros::ok())
 	{
+		//check for collisions
+		if(robot_control.check_for_collisions==1) 
+		{
+			//function trim laser down to only points in virtual corridor
+
+			//do ground removal on points in virtual corridor
+
+			//check for collision
+
+		}
+		else
+		{
+			//don't check for collision
+		}
+
+		//populate message
+		msg_CollisionOut.collision = rand(); //put result here
+		msg_CollisionOut.distance_to_collision = rand(); //put result here
+
+		//publish message
+		pub_col.publish(msg_CollisionOut);
 
 		loop_rate.sleep();
 		ros::spinOnce();
