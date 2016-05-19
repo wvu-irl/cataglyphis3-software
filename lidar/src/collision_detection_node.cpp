9d69dcee9d0cdb29bce4843ea30073e3c08fb0b1
Jared Strader <Jared.L.Strader@gmail.com>
2016-05-19 10:01:10 -0400

    add the 2nd layer of object avoidance detection

--- lidar/src/collision_detection_node.cpp
+++ lidar/src/collision_detection_node.cpp
@@ -161,26 +161,26 @@ private:
 			
 
 			// // // B: start method B: find
-			// // pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
-			// // pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
-			// // // Create the segmentation object
-			// // pcl::SACSegmentation<pcl::PointXYZ> seg;
-			// // // Optional
-			// // seg.setOptimizeCoefficients (true);
-			// // // Mandatory
-			// // seg.setModelType (pcl::SACMODEL_PLANE);
-			// // seg.setMethodType (pcl::SAC_RANSAC);
-			// // seg.setMaxIterations (1000);
-			// // seg.setDistanceThreshold (0.15); //Ground detection threshold parameter
-			// // seg.setInputCloud (cloud);
-			// // seg.segment (*inliers, *coefficients);
-			// // //pcl::ExtractIndices<pcl::PointXYZ> extract;
-			// // extract.setInputCloud (cloud);
-			// // extract.setIndices (inliers);
-			// // extract.setNegative (false);
-			// // extract.filter (*ground_filtered);
-			// // extract.setNegative (true);
-			// // extract.filter (*object_filtered);
+			pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+			pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+			// Create the segmentation object
+			pcl::SACSegmentation<pcl::PointXYZ> seg;
+			// Optional
+			seg.setOptimizeCoefficients (true);
+			// Mandatory
+			seg.setModelType (pcl::SACMODEL_PLANE);
+			seg.setMethodType (pcl::SAC_RANSAC);
+			seg.setMaxIterations (1000);
+			seg.setDistanceThreshold (0.15); //Ground detection threshold parameter
+			seg.setInputCloud (cloud);
+			seg.segment (*inliers, *coefficients);
+			pcl::ExtractIndices<pcl::PointXYZ> extract;
+			extract.setInputCloud (cloud);
+			extract.setIndices (inliers);
+			extract.setNegative (false);
+			extract.filter (*ground_filtered);
+			extract.setNegative (true);
+			extract.filter (*object_filtered);
 			// B: end
 
 			//check for collision
