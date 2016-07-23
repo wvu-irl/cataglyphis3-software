f08dd88ef815e37e985d7ce82e7a9f1beabcb7cb
chizhaoyang <cy0003@mix.wvu.edu>
2016-07-23 17:02:00 -0400

    add grid map publisher in keyframe node

---
+++ slam/include/map_layers.h
@@ -0,0 +1,33 @@
+#ifndef MAP_LAYERS_H
+#define MAP_LAYERS_H
+#include <string>
+#include <ros/ros.h>
+
+#define NUM_MAP_LAYERS 11
+#define MAP_SAT_LAYERS_START_INDEX 1
+#define MAP_SAT_LAYERS_END_INDEX 3
+#define MAP_KEYFRAME_LAYERS_START_INDEX 4
+#define MAP_KEYFRAME_LAYERS_END_INDEX 7
+enum MAP_LAYERS_T {_slope, _satDriveability, _satDriveabilityConf, _satObjectHeight, _keyframeDriveability, _keyframeDriveabilityConf, _keyframeObjectHeight,
+				   _reflectivity, _sampleProb, _roiNum, _keyframeWriteIntoGlobalMapSerialNum};
+enum DRIVEABILITY_T {_noObject, _passableOverhang, _impassable};
+inline std::string layerToString(MAP_LAYERS_T layer)
+{
+	switch(layer)
+	{
+	case _slope: return "slope";
+	case _satDriveability: return "satDriveability";
+	case _satDriveabilityConf: return "satDriveabilityConf";
+	case _satObjectHeight: return "satObjectHeight";
+	case _keyframeDriveability: return "keyframeDriveability";
+	case _keyframeDriveabilityConf: return "keyframeDriveabilityConf";
+	case _keyframeObjectHeight: return "keyframeObjectHeight";
+	case _reflectivity: return "reflectivity";
+	case _sampleProb: return "sampleProb";
+	case _roiNum: return "roiNum";
+	case _keyframeWriteIntoGlobalMapSerialNum: return "keyframeWriteIntoGlobalMapSerialNum";
+	default: ROS_FATAL("layerToString: unknown layer type");
+	}
+}
+
+#endif // MAP_LAYERS_H
