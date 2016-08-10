e59d3b17e663fddb95c61f0bcaf2bed17f3b6698
Jared Strader <Jared.L.Strader@gmail.com>
2016-08-09 20:06:35 -0400

    update calibration node

--- computer_vision/include/computer_vision/calibrate_camera.hpp
+++ computer_vision/include/computer_vision/calibrate_camera.hpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
-#include <iostream>
+#include <ros/package.h>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include <boost/filesystem.hpp>
+#include <boost/bind.hpp>
 
 #ifndef CALIBRATE_CAMERA_HPP
 #define CALIBRATE_CAMERA_HPP
@@ -9,20 +11,96 @@
 class CalibrateCamera
 {
 private:
-	cv::Mat _inputImage;
+
 public:
 	CalibrateCamera();
-	bool refreshImage(cv::Mat src);
-	void onTrackbar( int, void* );
 
-	/// Global Variables
-	const int alpha_slider_max = 100;
-	int alpha_slider;
-	double alpha;
-	double beta;
+	cv::Mat _dst;
+	cv::Mat _dstCopy;
+	cv::Mat _calibrationMask;
+	cv::Mat _saveMask;
 
-	/// Matrices to store images
-	cv::Mat dst;
+	int _robotWidthMax = 5792;
+	int _robotWidthSlider = 0;
+	int _robotHeightMax = 5792;
+	int _robotHeightSlider = 0;
+	int _robotShiftMax = 5792;
+	int _robotShiftSlider = 0;
+	int _radiusMax = 4095;
+	int _radiusSlider = 4095;
+	int _poleWidthMax = 5792;
+	int _poleWidthSlider = 0;
+	int _poleHeightMax = 5792/2;
+	int _poleHeightSlider = 0;
+
+	static void onTrackbarRobotWidth(int, void*){};
+	static void onTrackbarRobotHeight(int, void*){};
+	static void onTrackbarRobotShift(int, void*){};
+	static void onTrackbarRadius(int, void*){};
+	static void onTrackbarPoleWidth(int, void*){};
+	static void onTrackbarPoleHeight(int, void*){};
+
+	bool _showBody = false;
+	bool _showPole = false;
+	bool _showGrabber = false;
+	bool _showRadius = false;
+
+	static void bodyCheckBox(int state, void* object){
+		if(state==true)
+		{
+			((CalibrateCamera*)object)->_showBody=true;
+			ROS_INFO("True.");
+		}
+		else
+		{
+			((CalibrateCamera*)object)->_showBody=false;
+			ROS_INFO("False.");
+		}
+	};
+	static void poleCheckBox(int state, void* object){
+		if(state==true)
+		{
+			((CalibrateCamera*)object)->_showPole=true;
+		}
+		else
+		{
+			((CalibrateCamera*)object)->_showPole=false;
+		}
+	};
+	static void grabberCheckBox(int state, void* object){
+		if(state==true)
+		{
+			((CalibrateCamera*)object)->_showGrabber=true;
+		}
+		else
+		{
+			((CalibrateCamera*)object)->_showGrabber=false;
+		}
+	};
+	static void radiusCheckBox(int state, void* object){
+		if(state==true)
+		{
+			((CalibrateCamera*)object)->_showRadius=true;
+		}
+		else
+		{
+			((CalibrateCamera*)object)->_showRadius=false;
+		}
+	};
+	static void calibratePoseButton(int state, void* object){
+		ROS_WARN("Calibrate pose not implemented yet.");
+	}
+	static void updateImageButton(int state, void* object){
+		((CalibrateCamera*)object)->updateImage();
+	};
+	static void saveCalibrationButton(int state, void*){
+		ROS_WARN("Save not implemented yet.");
+	};
+
+	void displayImage();
+	void updateImage();
+	void loadCalibration();
+	void saveCalibration();
 };
 
 #endif //CALIBRATE_CAMERA_HPP
--- computer_vision/src/calibrate_camera_node.cpp
+++ computer_vision/src/calibrate_camera_node.cpp
@@ -1,68 +1,5 @@
-#include <ros/ros.h>
-#include <ros/package.h>
 #include <computer_vision/capture_class.hpp> 
 #include <computer_vision/calibrate_camera.hpp>
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <boost/filesystem.hpp>
-
-//Robot Width Image
-int robotWidthMax = 0;
-int robotWidthSlider = 0;
-
-//Robot Height in Image
-int robotHeightMax = 0;
-int robotHeightSlider = 0;
-
-//Robot Shift in Image
-int robotShiftMax = 0;
-int robotShiftSlider = 0;
-
-//Blocked Radius in Image
-int radiusMax = 0;
-int radiusSlider = 0;
-
-//Pole Width Image
-int poleWidthMax = 0;
-int poleWidthSlider = 0;
-
-//Pole Height Image
-int poleHeightMax = 0;
-int poleHeightSlider = 0;
-
-// Matrices to store images
-cv::Mat dst;
-cv::Mat dstCopy;
-
-void onTrackbarRobotWidth( int, void* )
-{
-	// ROS_INFO("robotWidthSlider = %i",robotWidthSlider);
-}
-
-void onTrackbarRobotHeight( int, void* )
-{
-	// ROS_INFO("robotHeightSlider = %i",robotHeightSlider);
-}
-
-void onTrackbarRobotShift( int, void* )
-{
-	// ROS_INFO("robotShiftSlider = %i",robotShiftSlider);
-}
-
-void onTrackbarRadius( int, void* )
-{
-	// ROS_INFO("radiusSlider = %i",radiusSlider);
-}
-
-void onTrackbarPoleWidth( int, void* )
-{
-	// ROS_INFO("poleWidthSlider = %i",poleWidthSlider);
-}
-
-void onTrackbarPoleHeight( int, void* )
-{
-	// ROS_INFO("poleHeightSlider = %i",poleHeightSlider);
-}
 
 int main(int argc, char **argv)
 {
@@ -75,67 +12,25 @@ int main(int argc, char **argv)
 	CalibrateCamera calibrateCamera;
 
 	//Capture Initial Image
-	//dst = cv::imread("/home/jared/cataglyphis_ws/src/computer_vision/samples.jpg");
-	//dstCopy = dst.clone();
+	calibrateCamera._dst = cv::imread("/home/jared/cataglyphis_ws/src/computer_vision/samples.jpg");
+	calibrateCamera._dstCopy = calibrateCamera._dst.clone();
 
-	if(capture.capture_image()==0)
-	{
-		ROS_ERROR("Could not capture initial image from camera. Terminating...");
-		return 0;
-	}
-	else
-	{
-		dst = capture.image_Mat.clone();
-		dstCopy = dst.clone();
-	}
-
-	//Set Maximum Values for Sliders
-	robotWidthMax = dst.cols;
-	robotHeightMax = dst.rows;
-	robotShiftMax = dst.rows;
-	radiusMax = sqrt((float)(robotWidthMax/2*robotWidthMax/2)+(float)(robotHeightMax/2*robotHeightMax/2));
-	radiusSlider = radiusMax;
-	poleWidthMax = dst.cols;
-	poleHeightMax = dst.rows/2;
-
-	//Create Windows
-	namedWindow("Current Image", cv::WINDOW_NORMAL);
-
-	//Create Trackbars
-	char TrackbarRobotWidth[50];
-	sprintf( TrackbarRobotWidth, "RW", robotWidthMax );
-	cv::createTrackbar( TrackbarRobotWidth, "Current Image", &robotWidthSlider, robotWidthMax, onTrackbarRobotWidth );
-
-	char TrackbarRobotHeight[50];
-	sprintf( TrackbarRobotHeight, "RH", robotHeightMax );
-	cv::createTrackbar( TrackbarRobotHeight, "Current Image", &robotHeightSlider, robotHeightMax, onTrackbarRobotHeight );
-
-	char TrackbarRobotShift[50];
-	sprintf( TrackbarRobotShift, "RS", robotShiftMax );
-	cv::createTrackbar( TrackbarRobotShift, "Current Image", &robotShiftSlider, robotShiftMax, onTrackbarRobotShift );
-
-	char TrackbarRadius[50];
-	sprintf( TrackbarRadius, "BR", radiusMax );
-	cv::createTrackbar( TrackbarRadius, "Current Image", &radiusSlider, radiusMax, onTrackbarRadius );
-
-	char TrackbarPoleWidth[50];
-	sprintf( TrackbarPoleWidth, "PW", poleWidthMax );
-	cv::createTrackbar( TrackbarPoleWidth, "Current Image", &poleWidthSlider, poleWidthMax, onTrackbarPoleWidth );
-
-	char TrackbarPoleHeight[50];
-	sprintf( TrackbarPoleHeight, "PH", poleHeightMax );
-	cv::createTrackbar( TrackbarPoleHeight, "Current Image", &poleHeightSlider, poleHeightMax, onTrackbarPoleHeight );
-
-	//Initialize calibration image
-	cv::Mat calibrationMask;
-	cv::Mat displayMask,blendMask,calibrationRGB,blended;
+	// if(capture.capture_image()==0)
+	// {
+	// 	ROS_ERROR("Could not capture initial image from camera. Terminating...");
+	// 	return 0;
+	// }
+	// else
+	// {
+	// 	dst = capture.image_Mat.clone();
+	// 	dstCopy = dst.clone();
+	// }
 
 	//Main Loop
 	while(ros::ok())
 	{
-		imshow("Current Image", dst);
-		calibrationMask = cv::Mat::zeros(5792,5792,CV_8U);
-
+		calibrateCamera.displayImage();
+		
 		char keyPress = cv::waitKey(30);
 		if(keyPress == 'r')
 		{
@@ -146,54 +41,20 @@ int main(int argc, char **argv)
 			}
 			else
 			{
-				dst = capture.image_Mat.clone();
-				dstCopy = dst.clone();
+				calibrateCamera._dst = capture.image_Mat.clone();
+				calibrateCamera._dstCopy = calibrateCamera._dst.clone();
 			}
 		}
 		else if(keyPress == 'u')
 		{
 			ROS_INFO("Updating image...");
-    		int limitRobotLeftWidth = (robotWidthMax - robotWidthSlider)/2;
-    		int limitRobotRightWidth = (robotWidthMax - robotWidthSlider)/2 + robotWidthSlider;
-    		int limitRobotHeightTop = robotHeightMax - robotHeightSlider - robotShiftSlider;
-    		int limitRobotHeightBottom = robotHeightMax - robotShiftSlider;
+			calibrateCamera.updateImage();
 
-    		int limitPoleLeftWidth = (poleWidthMax - poleWidthSlider)/2;
-    		int limitPoleRightWidth = (poleWidthMax - poleWidthSlider)/2 + poleWidthSlider;
-    		int limitPoleHeight = poleHeightMax*2 - poleHeightSlider;
-
-		    for(int i=0; i<calibrationMask.cols; i++)
-		    {
-		        for(int j=0; j<calibrationMask.rows; j++)
-		        {
-					int dist = sqrt( (i-(float)dst.cols/2)*(i-(float)dst.cols/2) + (j-(float)dst.rows/2)*(j-(float)dst.rows/2) );
-					if(dist < radiusSlider)
-					{
-						if(i < limitRobotLeftWidth || i > limitRobotRightWidth || j < limitRobotHeightTop || j > limitRobotHeightBottom)
-						{
-							if(i < limitPoleLeftWidth || i > limitPoleRightWidth || j < limitPoleHeight)
-							{
-								calibrationMask.at<uchar>(j,i)=255;
-							}
-						}
-					}
-		        }
-		    }
-
-		   //invert and threshold mask for display display masked image
-		    calibrationRGB = calibrationMask.clone();
-		    cv::cvtColor(calibrationRGB,calibrationRGB,CV_GRAY2RGB);
-		    cv::threshold(calibrationRGB,displayMask,0,255,0);
-			cv::threshold(calibrationRGB,blendMask,0,100,1);
-			blended = cv::Mat::zeros(5792,5792,CV_8U);
-			cv::add(dstCopy,blendMask,dst);
 		}
 		else if(keyPress == 's')
 		{
 		    ROS_INFO("Writing new mask to file...");
-			boost::filesystem::path P( ros::package::getPath("computer_vision") );
-			cv::imwrite(P.string() + "/data/images/calibration_mask.jpg",displayMask);
-			cv::imwrite(P.string() + "/data/images/calibration_mask_bak.jpg",displayMask);
+			calibrateCamera.saveCalibration();
 		}
 		else if(keyPress == 'q')
 		{
